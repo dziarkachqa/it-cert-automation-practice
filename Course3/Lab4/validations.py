d2561c2f23e74ec88844567f4288ccf913cbfeb4
Anastasia <dziarkach101@gmail.com>
2024-12-10 04:16:44 +0000

    Closes: #1.  Added forbitten char validation.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,9 +1,15 @@
+
 #!/usr/bin/env python3
 
 import re
 
+
+forbitten_char = ['.','_']
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
+
+    if username[0] in forbitten_char:
+        return False	
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
@@ -22,3 +28,10 @@ def validate_user(username, minlen):
 
 
 
+print(validate_user("blue.kale", 3)) # True
+
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+
+print(validate_user("red_quinoa", 4)) # True
+
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
